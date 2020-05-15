8f6d643c8ba3187cab0d32c25d2594fbd114e5a0
Ugochukwu Nwokoro <ugonwokoro@yahoo.com>
2020-05-15 13:25:15 +0100

    cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,25 +5,26 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    # @review = Review.new
   end
 
-  # def new
-  #   @cocktail = Cocktail.new
-  # end
+  def new
+    @cocktail = Cocktail.new
+  end
 
-  # def create
-  #   @cocktail = Cocktail.new(cocktail_params)
-  #   if @cocktail.save
-  #     redirect_to cocktail_path(@cocktail)
-  #   else
-  #     render "new"
-  #   end
-  # end
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
 
-  # # private
+  private
 
-  # def restaurant_params
-  #   params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-  # end
-
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
+  has_many :cocktails, through: :doses
   has_many :doses
 end
